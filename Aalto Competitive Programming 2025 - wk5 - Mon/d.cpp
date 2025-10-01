f994501e2f5fadc845c19dcc8d4f71bffd53d192
Thanh Duong <thanhduonghd114@gmail.com>
2025-10-02 00:01:41 +0300

    clean up

--- Aalto Competitive Programming 2025 - wk5 - Mon/d.cpp
+++ Aalto Competitive Programming 2025 - wk5 - Mon/d.cpp
@@ -18,7 +18,7 @@ struct PairHash {
 
 unordered_map<pair<int, int>, long long, PairHash> memo;
 
-long long backtracking(long long a[MAXN][MAXN], long long check[MAXN], long long i, long long depth, long long ans, long long n, int mask) {
+long long backtracking(long long i, long long depth, long long ans, int mask) {
     if (depth == n) return ans;
     
     auto key = make_pair(mask, i);
@@ -27,7 +27,7 @@ long long backtracking(long long a[MAXN][MAXN], long long check[MAXN], long long
     long long best = 1e18;
     for (int j = 1; j <= n; j++) {
         if (!(mask & (1 << j))) { 
-            best = min(best, backtracking(a, check, j, depth + 1, ans + a[i][j], n, mask | (1 << j)));
+            best = min(best, backtracking(j, depth + 1, ans + a[i][j], mask | (1 << j)));
         }
     }
     
@@ -42,11 +42,10 @@ void solve() {
             cin >> a[i][j];
         }
     }
-    for (int i = 1; i <= n; i++) check[i] = 0; 
-    
+
     for(int i = 1; i <= n; i++) {
         int mask = (1 << i);
-        result = min(result, backtracking(a, check, i, 1, a[i][i], n, mask));
+        result = min(result, backtracking(i, 1, a[i][i], mask));
     }
     
     cout << result;
